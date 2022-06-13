6c491dc36404f6871c147394007414af80d644b9
Avin Wilson <avinwilson54@gmail.com>
2022-06-13 17:12:51 +0530

    Final deployed version

--- script.js
+++ script.js
@@ -1,38 +1,34 @@
 let myLeads = [] ;
+let oldLeads = [] ;
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-log") ;
 const ulEl = document.getElementById("ul-el") ;
 const deleteBtn = document.getElementById("delete-btn") ;
-
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")) ;
-console.log(leadsFromLocalStorage)
+const tabBtn = document.getElementById("tab-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")) ;
 
 if (leadsFromLocalStorage)  {
      myLeads = leadsFromLocalStorage ;
-     renderLeads() ;
+     render(myLeads);
 }
-// console.log(Boolean(leadsFromLocalStorage)) ; to check whether the function is truthy or falsy value or using !! instead of boolean
 
-deleteBtn.addEventListener('dblclick', function() {
-    localStorage.clear() ;
-    myLeads = [] ;
-    renderLeads() ;
+tabBtn.addEventListener("click", function() {
+
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        myLeads.push(tabs[0].url) ;
+        localStorage.setItem("myLeads", JSON.stringify(myLeads)) ;
+        render(myLeads) ;
+    }) ;
+    
 }) ;
 
-inputBtn.addEventListener('click', () => {
-    myLeads.push(inputEl.value) ;
-    inputEl.value = "" ;
-    localStorage.setItem("myLeads", JSON.stringify(myLeads)) ;
-    renderLeads() ;
-}) ;
-
-function renderLeads() {
+function render(leads) {
     let listItems = "" ;
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
                     <li>
-                        <a href="${myLeads[i]}" target="_blank"> 
-                            ${myLeads[i]} 
+                        <a href="${leads[i]}" target="_blank"> 
+                            ${leads[i]} 
                         </a> 
                     </li>
                     `;
@@ -43,3 +39,16 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems ; // used this to improve the performance of the code, using innerHTML funcction we can create HTML elements.
 }
+
+deleteBtn.addEventListener('dblclick', function() {
+    localStorage.clear() ;
+    myLeads = [] ;
+    render(myLeads) ;
+}) ;
+
+inputBtn.addEventListener('click', () => {
+    myLeads.push(inputEl.value) ;
+    inputEl.value = "" ;
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) ;
+    render(myLeads) ;
+}) ;
